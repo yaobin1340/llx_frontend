012d04261379b98c2bce6e64016e0098e5fcc6c8
wanghaixiao <haixiao@yeah.net>
2016-12-20 15:00:18 +0800

    2016/12/20

--- app/app.js
+++ app/app.js
@@ -1,149 +1,60 @@
-angular.module( 'ohapp',
-    [
-        'ngResource',
-        'ipCookie',
-        'ui.router',
-        'ui.bootstrap',
-        'ohRoutes',
-        'ohConfig',
-    ]
-)
-.config( function config( $injector, $locationProvider)
-{
-    var $stateProvider = $injector.get( '$stateProvider' );
-    var $urlRouterProvider = $injector.get( '$urlRouterProvider' );
-    var $routesProvider = $injector.get( '$routesProvider' );
-    var $httpProvider = $injector.get( '$httpProvider' );
-    var $config = $injector.get('$configProvider').$get();
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
+var homeRouter = require('./server/homeRouter');
 
-    $urlRouterProvider.otherwise( '/home' );
+var app = express();
 
-    $httpProvider.interceptors.push('AuthInterceptor')
-    $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded;charset=utf-8";
-    //$httpProvider.defaults.withCredentials = true;
+// view engine setup
+app.set('views', path.join(__dirname, 'public'));
+app.set('view engine', 'ejs');
 
-    $locationProvider.html5Mode(true).hashPrefix('!');
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-    var routes = $routesProvider.routes;
+console.log('applicaiton running on port: ', process.env.PORT);
+app.all('*', homeRouter);
 
-    angular.forEach( routes, function( value, key )
-    {
-        $stateProvider.state( key, routes[ key ] );
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  res.redirect('/');
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
     });
-
-
-    /**
-     * The workhorse; converts an object to x-www-form-urlencoded serialization.
-     * @param {Object} obj
-     * @return {String}
-     */
-    var param = function(obj) {
-        var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
-
-        for(name in obj) {
-            value = obj[name];
-
-            if(value instanceof Array) {
-                for(i=0; i<value.length; ++i) {
-                    subValue = value[i];
-                    fullSubName = name + '[' + i + ']';
-                    innerObj = {};
-                    innerObj[fullSubName] = subValue;
-                    query += param(innerObj) + '&';
-                }
-            }
-            else if(value instanceof Object) {
-                for(subName in value) {
-                    subValue = value[subName];
-                    fullSubName = name + '[' + subName + ']';
-                    innerObj = {};
-                    innerObj[fullSubName] = subValue;
-                    query += param(innerObj) + '&';
-                }
-            }
-            else if(value !== undefined && value !== null)
-                query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
-        }
-
-        return query.length ? query.substr(0, query.length - 1) : query;
-    };
-
-    // Override $http service's default transformRequest
-    $httpProvider.defaults.transformRequest = [function(data) {
-        return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
-    }];
-})
-.directive("scroll", function ($window) {
-    return function(scope, element, attrs) {
-        angular.element($window).bind("scroll", function() {
-            if (this.pageYOffset >= 60) {
-                scope.showFloatMenu = true;
-            } else {
-                scope.showFloatMenu = false;
-            }
-            scope.$apply();
-        });
-    };
-})
-.run( function( $injector )
-{
-    var $rootScope = $injector.get( '$rootScope' );
-    var $state = $injector.get( '$state' );
-    var $stateParams = $injector.get( '$stateParams' );
-    var $session = $injector.get('$session');
-    $rootScope.$isLogin = false;
-
-    if ($session.get('auth').authToken) {
-        $rootScope.$isLogin = true;
-    }
-
-    $rootScope.$state = $state;
-    $rootScope.$stateParams = $stateParams;
-})
-.filter('trustHtml', function ($sce) {
-    return function (input) {
-        return $sce.trustAsHtml(input);
-    }
-})
-.filter('trustAsResourceUrl', function($sce) {
-    return function(input) {
-        return $sce.trustAsResourceUrl(input);
-    };
-})
-.filter('cut', function () {
-    return function (value, wordwise, max, tail) {
-        if (!value) return '';
-
-        max = parseInt(max, 10);
-        if (!max) return value;
-        if (value.length <= max) return value;
-
-        value = value.substr(0, max);
-        if (wordwise) {
-            var lastspace = value.lastIndexOf(' ');
-            if (lastspace != -1) {
-                value = value.substr(0, lastspace);
-            }
-        }
-
-        return value + (tail || ' …');
-    };
-})
-.animation('.slide', function() {
-    var NG_HIDE_CLASS = 'ng-hide';
-    return {
-        beforeAddClass: function(element, className, done) {
-            if(className === NG_HIDE_CLASS) {
-                element.slideUp(done);
-            }
-        },
-        removeClass: function(element, className, done) {
-            if(className === NG_HIDE_CLASS) {
-                element.hide().slideDown(done);
-            }
-        }
-    };
+  });
 }
-)
 
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
---
+++ app/provider/oh-routes-provider.js
@@ -0,0 +1,55 @@
+angular
+.module( 'ohRoutes', [] )
+.provider( '$routes', function $routesProvider()
+{
+    this.routes = {};
+
+    this.routes['main'] = {
+        url: '/',
+        abstract: true,
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+    };
+
+    this.routes[ 'main.home' ] =
+    {
+        url: 'home',
+        title: 'Home',
+        templateUrl: 'views/restaurants.html',
+        controller: 'RestaurantCtrl'
+    };
+
+
+    this.routes['signin'] = {
+        url : '/signin',
+        templateUrl : 'views/signin.html',
+        controller : 'SigninSignupCtrl'
+    };
+
+    this.routes['signup'] = {
+        url : '/signup',
+        templateUrl : 'views/signup.html',
+        controller : 'SigninSignupCtrl'
+    };
+
+    this.routes['forgot'] = {
+        url : '/forgot',
+        templateUrl : 'views/forgot-password.html',
+        // controller:'forgotCtrl'
+    };
+     this.routes['description'] = {
+        url : '/description',
+        templateUrl : 'views/descriptionProducts.html',
+        // controller:'descriptionCrtl'
+    };
+     this.routes['smallShop'] = {
+        url : '/smallShop',
+        templateUrl : 'views/smallShop.html',
+        // controller:'smallShopCrtl'
+    };
+    this.$get = function()
+    {
+        return( this.routes );
+    };
+}
+);
