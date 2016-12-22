c2ffd3b0ad4b337aa12c59e8520dd8deed8a6151
yaobin1340 <645894453@qq.com>
2016-12-22 10:00:27 +0800

    fix

--- app/app/app.js
+++ app/app/app.js
@@ -20,7 +20,7 @@ angular.module( 'ohapp',
     $urlRouterProvider.otherwise( '/home' );
 
     $httpProvider.interceptors.push('AuthInterceptor')
-    // $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded;charset=utf-8";
+    $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded;charset=utf-8";
     //$httpProvider.defaults.withCredentials = true;
 
     $locationProvider.html5Mode(true).hashPrefix('!');
@@ -31,6 +31,47 @@ angular.module( 'ohapp',
     {
         $stateProvider.state( key, routes[ key ] );
     });
+
+    /**
+     * The workhorse; converts an object to x-www-form-urlencoded serialization.
+     * @param {Object} obj
+     * @return {String}
+     */
+    var param = function(obj) {
+        var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+
+        for(name in obj) {
+            value = obj[name];
+
+            if(value instanceof Array) {
+                for(i=0; i<value.length; ++i) {
+                    subValue = value[i];
+                    fullSubName = name + '[' + i + ']';
+                    innerObj = {};
+                    innerObj[fullSubName] = subValue;
+                    query += param(innerObj) + '&';
+                }
+            }
+            else if(value instanceof Object) {
+                for(subName in value) {
+                    subValue = value[subName];
+                    fullSubName = name + '[' + subName + ']';
+                    innerObj = {};
+                    innerObj[fullSubName] = subValue;
+                    query += param(innerObj) + '&';
+                }
+            }
+            else if(value !== undefined && value !== null)
+                query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+        }
+
+        return query.length ? query.substr(0, query.length - 1) : query;
+    };
+
+    // Override $http service's default transformRequest
+    $httpProvider.defaults.transformRequest = [function(data) {
+        return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+    }];
 
 
 })
