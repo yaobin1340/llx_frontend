6a058a1869e72187880469c09a668b31cda56408
wanghaixiao <haixiao@yeah.net>
2017-01-19 16:23:09 +0800

    ceshi

--- controller/paysment-ctrl.js
+++ controller/paysment-ctrl.js
@@ -92,8 +92,8 @@ angular
             return theRequest;
         }
         if(!GetRequest().code){
-            var redirect_url = 'http://llx.51loveshow.com/home';
-            location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx1a060a56132dfff4&redirect_uri" 
+            var redirect_url = 'http://llx.51loveshow.com/payment';
+            location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx1a060a56132dfff4&redirect_uri=" 
 
         +encodeURIComponent(redirect_url)+"&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect"
         }else{
