5b73f7be9b5a51f35439e656666207b591d336ae
wanghaixiao <haixiao@yeah.net>
2017-01-20 12:04:35 +0800

    ceshi

--- controller/paysment-ctrl.js
+++ controller/paysment-ctrl.js
@@ -52,66 +52,65 @@ angular
                     })
         };  
         function zhifu(){
-            function onBridgeReady(){
-               WeixinJSBridge.invoke(
-                   'getBrandWCPayRequest', {
-                        "appId":$scope.data.appId,     //公众号名称，由商户传入     
-                        "timeStamp":$scope.data.timeStamp,         //时间戳，自1970年以来的秒数     
-                        "nonceStr":$scope.data.nonceStr, //随机串     
-                        "package":$scope.data.package,     
-                        "signType":$scope.data.signType,         //微信签名方式：     
-                        "paySign":$scope.data.paySign //微信签名 
-                   },
-                   function(res){ 
-                   alert(res.err_msg);
-                       if(res.err_msg == "get_brand_wcpay_request：ok" ) {
-                            //完成支付，返回后台数据
+            // function onBridgeReady(){
+            //    WeixinJSBridge.invoke(
+            //        'getBrandWCPayRequest', {
+            //             "appId":$scope.data.appId,     //公众号名称，由商户传入     
+            //             "timeStamp":$scope.data.timeStamp,         //时间戳，自1970年以来的秒数     
+            //             "nonceStr":$scope.data.nonceStr, //随机串     
+            //             "package":$scope.data.package,     
+            //             "signType":$scope.data.signType,         //微信签名方式：     
+            //             "paySign":$scope.data.paySign //微信签名 
+            //        },
+            //        function(res){ 
+            //        alert(res.err_msg);
+            //            if(res.err_msg == "get_brand_wcpay_request：ok" ) {
+            //                 //完成支付，返回后台数据
 
-                       }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
-                   }
-               ); 
-            }
-            if (typeof WeixinJSBridge == "undefined"){
-               if( document.addEventListener ){
-                   document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-               }else if (document.attachEvent){
-                   document.attachEvent('WeixinJSBridgeReady', onBridgeReady); 
-                   document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
-               }
-            }else{
-               onBridgeReady();
-            } 
+            //            }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
+            //        }
+            //    ); 
+            // }
+            // if (typeof WeixinJSBridge == "undefined"){
+            //    if( document.addEventListener ){
+            //        document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+            //    }else if (document.attachEvent){
+            //        document.attachEvent('WeixinJSBridgeReady', onBridgeReady); 
+            //        document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+            //    }
+            // }else{
+            //    onBridgeReady();
+            // } 
 
             //支付
-            // wxConfig();
-            // wx.ready(function() {
-            //     console.log($scope.data.timeStamp);
-            //     wx.chooseWXPay({
-            //         timestamp: $scope.data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-            //         nonceStr: $scope.data.nonceStr, // 支付签名随机串，不长于 32 位
-            //         package: $scope.data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-            //         signType: $scope.data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-            //         paySign: $scope.data.paySign, // 支付签名
-            //         success: function (res) {
-            //             // 支付成功后的回调函数
-            //            console.log(res+"支付成功") 
-            //         }
-            //     })
-            // })
+            wxConfig();
+            wx.ready(function() {
+                wx.chooseWXPay({
+                    timestamp: $scope.data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+                    nonceStr: $scope.data.nonceStr, // 支付签名随机串，不长于 32 位
+                    package: $scope.data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+                    signType: $scope.data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+                    paySign: $scope.data.paySign, // 支付签名
+                    success: function (res) {
+                        // 支付成功后的回调函数
+                       console.log(res+"支付成功") 
+                    }
+                })
+            })
         }
-    // function wxConfig(){
-    //     $.getJSON($config.api_uri +'/Apipublic/Apilogin/get_wxconfig',function(data){
-    //         wx.config({
-    //             debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-    //             appId: data.wxappId, // 必填，公众号的唯一标识
-    //             timestamp: data.wxtimestamp, // 必填，生成签名的时间戳
-    //             nonceStr: data.wxnonceStr, // 必填，生成签名的随机串
-    //             signature: data.wxsignature,// 必填，签名，见附录1
-    //             jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-    //         });
+    function wxConfig(){
+        $.getJSON($config.api_uri +'/Apipublic/Apilogin/get_wxconfig',function(data){
+            wx.config({
+                debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                appId: data.wxappId, // 必填，公众号的唯一标识
+                timestamp: data.wxtimestamp, // 必填，生成签名的时间戳
+                nonceStr: data.wxnonceStr, // 必填，生成签名的随机串
+                signature: data.wxsignature,// 必填，签名，见附录1
+                jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+            });
 
-    //     });
-    // }
+        });
+    }
 
 
 
