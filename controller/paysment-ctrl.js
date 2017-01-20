a2141bddd8e9b2a8c36a6b1aae478eb2de89341d
wanghaixiao <haixiao@yeah.net>
2017-01-20 10:38:30 +0800

    ceshi

--- controller/paysment-ctrl.js
+++ controller/paysment-ctrl.js
@@ -27,7 +27,6 @@ angular
                     url: 'http://be.51loveshow.com/Apipublic/WxPay/get_openidbycode',
                     data:{code:GetRequest().code}
                 }).success(function (data) {
-                    console.log(data)
                     $scope.openid=data.openid;
                 })
         }
@@ -56,32 +55,35 @@ angular
         };  
         function zhifu(){
             //支付
-            alert("开始支付")
-            wx.chooseWXPay({
-                timestamp: data.wxtimestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-                nonceStr: data.wxnonceStr, // 支付签名随机串，不长于 32 位
-                package: data.wxpackage, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-                signType: data.wxsignType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-                paySign: data.wxpaySign, // 支付签名
-                success: function (res) {
-                    // 支付成功后的回调函数
-                   console.log(res+"支付成功") 
-                }
-            })
+            wxConfig();
+            console.log("开始支付");
+            wx.ready(function() {
+                wx.chooseWXPay({
+                    timestamp: $scope.data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+                    nonceStr: $scope.data.nonceStr, // 支付签名随机串，不长于 32 位
+                    package: $scope.data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+                    signType: $scope.data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+                    paySign: $scope.data.paySign, // 支付签名
+                    success: function (res) {
+                        // 支付成功后的回调函数
+                       console.log(res+"支付成功") 
+                    }
+                })
+            }
         }
-    // function wxConfig(){
-    //     $.getJSON($config.api_uri +'/Apipublic/Apilogin/get_wxconfig',function(data){
-    //         wx.config({
-    //             debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-    //             appId: data.wxappId, // 必填，公众号的唯一标识
-    //             timestamp: data.wxtimestamp, // 必填，生成签名的时间戳
-    //             nonceStr: data.wxnonceStr, // 必填，生成签名的随机串
-    //             signature: data.wxsignature,// 必填，签名，见附录1
-    //             jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-    //         });
+    function wxConfig(){
+        $.getJSON($config.api_uri +'/Apipublic/Apilogin/get_wxconfig',function(data){
+            wx.config({
+                debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                appId: data.wxappId, // 必填，公众号的唯一标识
+                timestamp: data.wxtimestamp, // 必填，生成签名的时间戳
+                nonceStr: data.wxnonceStr, // 必填，生成签名的随机串
+                signature: data.wxsignature,// 必填，签名，见附录1
+                jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+            });
 
-    //     });
-    // }
+        });
+    }
 
 
 
