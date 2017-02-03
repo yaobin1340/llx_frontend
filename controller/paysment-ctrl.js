156d4e9790d2fc93212cd6e5d7246d5092c7b56b
wanghaixiao <haixiao@yeah.net>
2017-02-03 14:59:59 +0800

    ceshi

--- controller/paysment-ctrl.js
+++ controller/paysment-ctrl.js
@@ -11,18 +11,13 @@ angular
         var $mdMedia = $injector.get('$mdMedia');
         var $mdToast = $injector.get('$mdToast');
 
+        $scope.order_id = $session.get('order_id');
+        $scope.need_pay = $session.get('need_pay');
+        $scope.log_id = $session.get('log_id');
         
-        if(JSON.stringify($session.get('order_id'))=='{}'){
-           location.replace(location.href);
-           return;
-         }else{
-          $scope.order_id=$session.get('order_id');
-          $scope.need_pay=$session.get('need_pay');
-          $scope.log_id=$session.get('log_id');
-         }
         //点击支付按钮
         $scope.zhifu = function(){
-            chosepay()
+            chosepay();
         }
           
         //获取构建参数信息
