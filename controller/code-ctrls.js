7eb8527b16101a3a2b4057ecdb784cf5180202a5
wanghaixiao <haixiao@yeah.net>
2017-02-11 13:46:39 +0800

    ceshi

--- controller/code-ctrls.js
+++ controller/code-ctrls.js
@@ -19,9 +19,9 @@ angular
                     url: 'http://be.51loveshow.com/Apipublic/WxPay/get_openidbycode',
                     data:{code:GetRequest().code}
                 }).success(function (data) {
+                    alert(data);
                     // $session.set("code",data.openid);
                     sessionStorage.setItem('code',data.openid);
-                    alert(data.openid);
                     $state.go("payment");
                 })
         }
