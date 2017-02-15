f196d63e3bbea751e3ba7955617d7c5472da6892
wanghaixiao <haixiao@yeah.net>
2017-02-15 20:20:32 +0800

    ceshi

--- controller/description-ctrls.js
+++ controller/description-ctrls.js
@@ -23,9 +23,8 @@ angular
 		$scope.promise = null;
         	//获取商家详情页
         if($stateParams.type==1){
-        	alert($stateParams.img_url);
         	$scope.promise=$http
-				.post($stateParams.img_url,{shop_id:$scope.shops_id,openid:$stateParams.openid})
+				.post($stateParams.img_url,{shop_id:$stateParams.shops_id,openid:$stateParams.openid})
 				.success(function (data) {
 					if(datas.success){
                         $scope.detail=datas.detail;
