153ff919ee2cfd8a6f25918db262f57b8d831a79
wanghaixiao <haixiao@yeah.net>
2017-02-17 13:16:32 +0800

    ceshi

--- controller/integralSubmit-ctrls.js
+++ controller/integralSubmit-ctrls.js
@@ -68,7 +68,7 @@ angular
                 })      
 
         $scope.notcut = function(){
-            if($scope.needgold>0){
+            if($scope.needgold>=0||$scope.needgold==''){
                 $scope.needgold=$scope.needgold.replace(/(\.\d{2})\d*$/,'\$1');
                 $scope.needgold=$scope.needgold>$scope.gold?$scope.gold:$scope.needgold;
                 $scope.needgold=$scope.needgold>$scope.prices?$scope.prices:$scope.needgold;
