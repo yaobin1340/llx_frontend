e78fc100ac86e8cc3b0e30fd83d69e6375568c13
wanghaixiao <haixiao@yeah.net>
2017-03-03 16:24:19 +0800

    ceshi

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,44 @@ var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
 var csso = require('gulp-csso');
 var debug = require('gulp-strip-debug');
+var imagemin = require('gulp-imagemin');
+var htmlmin = require('gulp-htmlmin');
 
 gulp.task('default',function() {
+	gulp.run('img');  
+    gulp.run('html');  
+    gulp.run('js.css');  
+});
+//压缩图片
+gulp.task('img',function(){
+	gulp.src('assets/img/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('assets/img'))
+});
+
+//压缩html代码
+gulp.task('html',function(){
+	var options = {
+		collapseWhitespace:true,
+		collapseBooleanAttributes:true,
+		removeComments:true,
+		removeEmptyAttributes:true,
+		removeScriptTypeAttributes:true,
+		removeStyleLinkTypeAttributes:true,
+		minifyJS:true,
+		minifyCSS:true
+	};
+	gulp.src('views/*.html')
+		.pipe(htmlmin(options))
+		.pipe(gulp.dest('views/'));
+});
+
+//压缩js、css文件
+gulp.task('js.css',function() {
 	var jsFilters = filter(['**/*.js'],{restore:true});
 	var cssFilter = filter('**/*.css',{restore:true});
 	var indexHtmlFilter = filter(['**/*','!**/index.html'],{restore:true});
-	return gulp.src('index.html')
+	return gulp.src(['index.html'])
 		.pipe(jshint())
 		.pipe(useref())
 		.pipe(jsFilters)
@@ -28,26 +60,5 @@ gulp.task('default',function() {
 		.pipe(rev())
 		.pipe(indexHtmlFilter.restore)
 		.pipe(revReplace())
-		.pipe(gulp.dest('../llx_frontend'));
+		.pipe(gulp.dest('../llx_frontend'))
 });
-
-  // gulp.task('controller',function() {
-  // 	var jsFilters = filter('/contro/*.js',{restore:true});
-  // 	var cssFilter = filter('**/*.css',{restore:true});
-  // 	var indexHtmlFilter = filter(['**/*','!**/index.html'],{restore:true});
-  // 	return gulp.src('index.html')
-  // 		.pipe(jshint())
-  // 		.pipe(useref())
-  // 		.pipe(jsFilters)
-	 // 	.pipe(debug())
-  // 		.pipe(uglify())
-  // 		.pipe(jsFilters.restore)
-  // 		.pipe(cssFilter)
-  // 		.pipe(csso())
-  // 		.pipe(cssFilter.restore)
-  // 		.pipe(indexHtmlFilter)
-  // 		.pipe(rev())
-  // 		.pipe(indexHtmlFilter.restore)
-  // 		.pipe(revReplace())
-  // 		.pipe(gulp.dest('../llx_frontend'));
-  // });
