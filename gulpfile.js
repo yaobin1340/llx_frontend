dc25582fa01b8e28a9db11409abdc16060f51350
wanghaixiao <haixiao@yeah.net>
2017-03-03 10:52:32 +0800

    ceshi

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,53 @@
- var gulp = require('gulp');
- var rev = require('gulp-rev');
- var revReplace = require('gulp-rev-replace');
- var useref = require('gulp-useref');
- var filter = require('gulp-filter');
- var uglify = require('gulp-uglify');
- var jshint = require('gulp-jshint');
- var csso = require('gulp-csso');
- var debug = require('gulp-strip-debug');
- gulp.task('default',function() {
- 	var jsFilter = filter('/resou/*.js',{restore:true});
- 	var cssFilter = filter('**/*.css',{restore:true});
- 	var indexHtmlFilter = filter(['**/*','!**/index.html'],{restore:true});
- 	return gulp.src('index.html')
- 		.pipe(jshint())
- 		.pipe(useref())
- 		.pipe(jsFilter)
-		.pipe(debug())
- 		.pipe(uglify())
- 		.pipe(jsFilter.restore)
- 		.pipe(cssFilter)
- 		.pipe(csso())
- 		.pipe(cssFilter.restore)
- 		.pipe(indexHtmlFilter)
- 		.pipe(rev())
- 		.pipe(indexHtmlFilter.restore)
- 		.pipe(revReplace())
- 		.pipe(gulp.dest('../llx_frontend'));
- });
+var gulp = require('gulp');
+var rev = require('gulp-rev');
+var revReplace = require('gulp-rev-replace');
+var useref = require('gulp-useref');
+var filter = require('gulp-filter');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var csso = require('gulp-csso');
+var debug = require('gulp-strip-debug');
 
- gulp.task('controller',function() {
- 	var jsFilters = filter('/contro/*.js',{restore:true});
- 	var cssFilter = filter('**/*.css',{restore:true});
- 	var indexHtmlFilter = filter(['**/*','!**/index.html'],{restore:true});
- 	return gulp.src('index.html')
- 		.pipe(jshint())
- 		.pipe(useref())
- 		.pipe(jsFilters)
+gulp.task('default',function() {
+	var jsFilters = filter(['**/*.js'],{restore:true});
+	var cssFilter = filter('**/*.css',{restore:true});
+	var indexHtmlFilter = filter(['**/*','!**/index.html'],{restore:true});
+	return gulp.src('index.html')
+		.pipe(jshint())
+		.pipe(useref())
+		.pipe(jsFilters)
 		.pipe(debug())
- 		.pipe(uglify())
- 		.pipe(jsFilters.restore)
- 		.pipe(cssFilter)
- 		.pipe(csso())
- 		.pipe(cssFilter.restore)
- 		.pipe(indexHtmlFilter)
- 		.pipe(rev())
- 		.pipe(indexHtmlFilter.restore)
- 		.pipe(revReplace())
- 		.pipe(gulp.dest('../llx_frontend'));
- });
+		.pipe(uglify({
+			mangle: false //是否修改变量名
+		}))
+		.pipe(jsFilters.restore)
+		.pipe(cssFilter)
+		.pipe(csso())
+		.pipe(cssFilter.restore)
+		.pipe(indexHtmlFilter)
+		.pipe(rev())
+		.pipe(indexHtmlFilter.restore)
+		.pipe(revReplace())
+		.pipe(gulp.dest('../llx_frontend'));
+});
+
+  // gulp.task('controller',function() {
+  // 	var jsFilters = filter('/contro/*.js',{restore:true});
+  // 	var cssFilter = filter('**/*.css',{restore:true});
+  // 	var indexHtmlFilter = filter(['**/*','!**/index.html'],{restore:true});
+  // 	return gulp.src('index.html')
+  // 		.pipe(jshint())
+  // 		.pipe(useref())
+  // 		.pipe(jsFilters)
+	 // 	.pipe(debug())
+  // 		.pipe(uglify())
+  // 		.pipe(jsFilters.restore)
+  // 		.pipe(cssFilter)
+  // 		.pipe(csso())
+  // 		.pipe(cssFilter.restore)
+  // 		.pipe(indexHtmlFilter)
+  // 		.pipe(rev())
+  // 		.pipe(indexHtmlFilter.restore)
+  // 		.pipe(revReplace())
+  // 		.pipe(gulp.dest('../llx_frontend'));
+  // });
